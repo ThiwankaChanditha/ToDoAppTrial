b12a5a3622eda7569de46daf13dc12c55bea4ae6
Thiwanka Chanditha <thiwankachandithasinhalage@gmail.com>
2024-09-08 10:56:28 +0000

    added necessary files

---
+++ components/TodoItem.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import { View, Text, CheckBox, TouchableOpacity, StyleSheet } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+
+const styles = StyleSheet.create({
+  todoItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginVertical: 5,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 2,
+    marginLeft: 10,
+  },
+  text: {
+    flex: 1,
+    fontSize: 18,
+    color: '#333',
+    marginLeft: 8,
+  },
+  completedText: {
+    textDecorationLine: 'line-through',
+    color: '#888',
+  },
+  button: {
+    marginLeft: 10,
+    backgroundColor: '#007BFF',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 12,
+  },
+  moveButton: {
+    backgroundColor: '#4CAF50',
+  },
+  deleteButton: {
+    backgroundColor: '#FF6347',
+  },
+  restoreButton: {
+    backgroundColor: '#FF7F50',
+  },
+  editButton: {
+    backgroundColor: '#2671eb',
+  },
+});
+
+export default function TodoItem({
+  task,
+  deleteTask,
+  toggleCompleted,
+  startEditTask,
+  moveUp,
+  moveDown,
+  pinTask,
+  unpinTask,
+  showUnpinButton,
+  restoreTask,
+  showRestoreButton,
+}) {
+  return (
+    <View style={styles.todoItem}>
+      <CheckBox value={task.completed} onValueChange={() => toggleCompleted(task.id)} />
+      <Text style={[styles.text, task.completed && styles.completedText]}>
+        {task.text}
+      </Text>
+      {!task.completed && !showRestoreButton && (
+        <>
+          <TouchableOpacity style={[styles.button, styles.moveButton]} onPress={() => moveUp(task.id)}>
+            <Text style={styles.buttonText}>↑</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[styles.button, styles.moveButton]} onPress={() => moveDown(task.id)}>
+            <Text style={styles.buttonText}>↓</Text>
+          </TouchableOpacity>
+
+          {showUnpinButton ? (
+             <TouchableOpacity onPress={() => unpinTask(task.id)}>
+                <MaterialIcons
+                  name="push-pin"
+                  size={24}
+                  color="red"
+                  style={{ transform: [{ rotate: '45deg' }] }}
+                />
+              </TouchableOpacity>
+          ) : (
+
+            <TouchableOpacity onPress={() => pinTask(task.id)}>
+              <MaterialIcons 
+                name="push-pin" 
+                size={24}
+                marginLeft={20} 
+                color="black" />
+            </TouchableOpacity>
+          )}
+        </>
+      )}
+      {showRestoreButton && (
+        <TouchableOpacity style={[styles.button, styles.restoreButton]} onPress={restoreTask}>
+          <Text style={styles.buttonText}>Restore</Text>
+        </TouchableOpacity>
+      )}
+
+      <TouchableOpacity style={[styles.button, styles.deleteButton]} onPress={() => deleteTask(task.id)}>
+        <Text style={styles.buttonText}>Delete</Text>
+      </TouchableOpacity>
+
+      {!task.completed && !showRestoreButton && (
+        <TouchableOpacity 
+          style={[styles.button, styles.editButton]} 
+          onPress={() => startEditTask(task.id)}>
+          <Text style={styles.buttonText}>Edit</Text>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+}
---
+++ components/TodoList.js
@@ -0,0 +1,275 @@
+import React, { useState } from 'react';
+import { View, TextInput, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import TodoItem from './TodoItem';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+  },
+  contentContainer: {
+    flex: 1,
+    width: '100%',
+    maxWidth: 500,
+    height: 200,
+    marginBottom: 20,
+  },
+  header: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 15,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+    marginBottom: 40,
+    position: 'absolute',
+    bottom: '8%',
+    width: '100%',
+  },
+  textInput: {
+    flex: 1,
+    fontSize: 18,
+    padding: 10,
+    color: '#333',
+  },
+  addButton: {
+    marginLeft: 10,
+    backgroundColor: '#4CAF50',
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  sectionTitle: {
+    textAlign: 'center',
+    color: '#A52345',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+  toggleButton: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 10,
+    width: 100,
+  },
+  toggleButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  emptyMessage: {
+    color: '#888',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 10,
+    marginVertical: '4%',
+  },
+});
+
+export default function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [text, setText] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+  const [editTaskId, setEditTaskId] = useState(null);
+  const [viewPinned, setViewPinned] = useState(false);
+  const [viewCompleted, setViewCompleted] = useState(false);
+  
+  function addTask() {
+    if (isEditing) {
+      setTasks(tasks.map(task => (task.id === editTaskId ? { ...task, text } : task)));
+      setIsEditing(false);
+      setEditTaskId(null);
+    } else {
+      const newTask = { id: Date.now(), text, completed: false, pinned: false };
+     if(text.trim() !== ""){
+        setTasks([...tasks, newTask]);
+      }
+    }
+    setText('');
+  }
+
+  function deleteTask(id) {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
+  function toggleCompleted(id) {
+    setTasks(tasks.map(task => 
+      task.id === id ? { ...task, completed: !task.completed } : task
+    ));
+  }
+
+  function startEditTask(id) {
+    const taskToEdit = tasks.find(task => task.id === id);
+    setText(taskToEdit.text);
+    setIsEditing(true);
+    setEditTaskId(id);
+  }
+
+  function moveUp(id) {
+    const index = tasks.findIndex(task => task.id === id);
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
+  }
+
+  function moveDown(id) {
+    const index = tasks.findIndex(task => task.id === id);
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
+  }
+
+  function pinTask(id) {
+    setTasks(tasks.map(task => 
+      task.id === id ? { ...task, pinned: true } :task
+    ));
+  }
+
+  function unpinTask(id) {
+    setTasks(tasks.map(task => 
+      task.id === id ? { ...task, pinned: false } : task
+    ));
+  }
+
+  function restoreTask(id) {
+    toggleCompleted(id);
+  }
+
+  function toggleView() {
+    setViewPinned(!viewPinned);
+  }
+
+  function toggleViewCompleted() {
+    setViewCompleted(!viewCompleted);
+  }
+
+  const pinnedTasks = tasks.filter(task => task.pinned);
+  const completedTasks = tasks.filter(task => task.completed);
+  const activeTasks = tasks.filter(task => !task.completed && !task.pinned);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text style={styles.header}>To do List</Text>
+
+        {!viewPinned && !viewCompleted ? (
+          <>
+            <FlatList
+              data={activeTasks}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <TodoItem
+                  task={item}
+                  deleteTask={deleteTask}
+                  toggleCompleted={toggleCompleted}
+                  startEditTask={startEditTask}
+                  moveUp={() => moveUp(item.id)}
+                  moveDown={() => moveDown(item.id)}
+                  pinTask={() => pinTask(item.id)}
+                />
+              )}
+              ListEmptyComponent={<Text style={styles.emptyMessage}>No tasks available.</Text>}
+            />
+
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.textInput}
+                value={text}
+                onChangeText={setText}
+                placeholder="New Task"
+                placeholderTextColor="#999"
+              />
+              <TouchableOpacity style={styles.addButton} onPress={addTask}>
+                <Text style={styles.addButtonText}>{isEditing ? 'Update' : 'Add'}</Text>
+              </TouchableOpacity>
+            </View>
+
+           <View style={styles.buttonsContainer}>
+            <TouchableOpacity style={styles.toggleButton} onPress={toggleView}>
+              <Text style={styles.toggleButtonText}>Pinned</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.toggleButton} onPress={toggleViewCompleted}>
+              <Text style={styles.toggleButtonText}>Completed</Text>
+            </TouchableOpacity>
+        </View>
+          </>
+        ) : viewPinned ? (
+          <>
+            <Text style={styles.sectionTitle}>Pinned Tasks</Text>
+            <FlatList
+              data={pinnedTasks}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <TodoItem
+                  task={item}
+                  deleteTask={deleteTask}
+                  unpinTask={() => unpinTask(item.id)}
+                  showUnpinButton
+                />
+              )}
+              ListEmptyComponent={<Text style={styles.emptyMessage}>No pinned tasks available.</Text>}
+            />
+
+            <TouchableOpacity style={styles.toggleButton} onPress={toggleView}>
+              <Text style={styles.toggleButtonText}>Back</Text>
+            </TouchableOpacity>
+          </>
+        ) : (
+          <>
+            <Text style={styles.sectionTitle}>Completed Tasks</Text>
+            <FlatList
+              data={completedTasks}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <TodoItem
+                  task={item}
+                  deleteTask={deleteTask}
+                  restoreTask={() => restoreTask(item.id)}
+                  showRestoreButton
+                />
+              )}
+              ListEmptyComponent={<Text style={styles.emptyMessage}>No completed tasks available.</Text>}
+            />
+
+            <TouchableOpacity style={styles.toggleButton} onPress={toggleViewCompleted}>
+              <Text style={styles.toggleButtonText}>Back</Text>
+            </TouchableOpacity>
+          </>
+        )}
+      </View>
+    </View>
+  );
+}
